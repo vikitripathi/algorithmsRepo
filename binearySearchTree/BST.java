4c6c965e332f904631466ed9f28a98d9a940ca45
Abhishek Dutt <abhishek09011993@gmail.com>
2014-12-14 18:49:14 +0530

    feat: Implemeted key traversing , rank , floor and size in the BST

--- binearySearchTree/BST.java
+++ binearySearchTree/BST.java
@@ -14,13 +14,46 @@ public class BST<Key extends Comparable<Key>, Value> {
 		private Key key;
 		private Value val;
 		private Node left, right;
+		private int count;//no of nodes in subtree
 		public Node(Key key, Value val)
 		{
 			this.key = key;
 			this.val = val;
 		}
 	}
-     //insert
+	//count no of nodes in a tree
+	public int size()
+	{
+	  return size(root);
+	}
+
+	 private int size(Node x)
+	 {
+	   if (x == null) return 0;
+	   return x.count;
+	 }
+	 
+	//Inorder traversal
+	/*
+	・Traverse left subtree.
+	  Enqueue key.
+        ・Traverse right subtree.
+	*/
+	//get keys in ascending order
+	 public Iterable<Key> keys()
+	 {
+	         Queue<Key> q = new Queue<Key>(); 
+		 inorder(root, q); 
+		 return q;	
+	 }
+	private void inorder(Node x, Queue<Key> q) 
+	{ 
+		 if (x == null) return; 
+		 inorder(x.left, q); 
+		 q.enqueue(x.key); 
+		 inorder(x.right, q); 
+	}
+	//insert
 	//if key in the tree, then reset value
 	//else add new node at the required place
 	//client insert method!!!
@@ -41,8 +74,22 @@ public class BST<Key extends Comparable<Key>, Value> {
 		 else
 		 if (cmp == 0)
 		 x.val = val;
+		 x.count = 1 + size(x.left) + size(x.right);
 		 return x;
 	 }
+	 public int rank(Key key) 
+        {
+            return rank(key, root); 
+        }
+        private int rank(Key key, Node x) 
+        { 
+         if (x == null) return 0; 
+         int cmp = key.compareTo(x.key);
+         if (cmp < 0) return rank(key, x.left); 
+         else if (cmp > 0) return 1 + size(x.left) + rank(key, x.right); 
+         else if (cmp == 0) return size(x.left); 
+        }
+        
 	 //search a key 
 	 //no of compares = 1+ depth of node
 	 public Value get(Key key)
@@ -64,10 +111,26 @@ public class BST<Key extends Comparable<Key>, Value> {
 //	 {
 //	 /* see next slides */
 //	 }
-//	 public Iterable<Key> iterator()
-//	 {
-//	 /* see next slides */
-//	 }
+
+
+	//computing floor and ceil
+	public Key floor(Key key)
+	{ 
+	 Node x = floor(root, key);
+	 if (x == null) return null;
+	 return x.key;
+	}
+	
+	private Node floor(Node x, Key key)
+	{ 
+	 if (x == null) return null;
+	 int cmp = key.compareTo(x.key);
+	 if (cmp == 0) return x;
+	 if (cmp < 0) return floor(x.left, key);
+	 Node t = floor(x.right, key);
+	 if (t != null) return t;
+	 else return x;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
